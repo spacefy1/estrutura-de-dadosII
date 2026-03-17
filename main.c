3a4f1daa6d3b0fa4bb91fba02a6c999bb557a8cb
spacefy1 <161043737+spacefy1@users.noreply.github.com>
2026-03-17 08:54:24 -0300

    Modify Arv struct fields and fix malloc syntax
    Updated struct Arv to use unsigned int for qtdChaves and added posicao field.

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 // insercao usa recursao
@@ -7,21 +6,26 @@
 // importar no pontoH que so tem o cabeçalho das funcoes 
 // buscar é procurar pelo no/chave na arvore, parametro arvore e no 
 // malloc é priomeiro desalocar a folha e dps o pai, o free é de baixo pra cima 
+// esse arqvuivo é onde vou colocar o struct e tem um .h que seja um header
+// o header é como uma interface
 
 typedef struct Arv {
-    int qtdChaves; // quantidade de chaves 
+    unsigned int qtdChaves; // quantidade de chaves 
     int chave [ORDEM - 1];
     struct Arv* filho[ORDEM];  // sempre um a mais do que a chave do de cima
     int posOrigem[ORDEM];  // posicao da chaves do arquivo final 
+    unsigned int posicao;
+    int folha;
 }Arv;
 
-/* struct criarArv(struct Arvb){
+struct criarArv(int folha){
    if (Arv == NULL){
        printf("Digite um valor real");
    } 
    
-   
-} */
+   struct Arv *novoNo = (strut novoNo *)malloc(sizeof(struct novoNo));
+
+}
 
 int main()
 {
